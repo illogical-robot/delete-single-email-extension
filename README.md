1119f67fcf94da751161617c42cb752074000cd7
Corbin Davenport <me@corbin.io>
2017-12-23 23:11:06 -0500

    Upload version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# delete-single-email-extension
-Adds keyboard shortcut support for deleting single emails in Gmail
+# Delete Key for Gmail™
+This extension adds a keyboard shortcut for deleting a single email in Gmail. It performs the same action as clicking the menu button on a message and selecting 'Delete this message.' This extension is especially helpful for deleting single emails in a thread, since Gmail offers no keyboard shortcut for that action.
+
+The default shortcut is `SHIFT + 5`, but it can be changed from the options page.
+
+---------------------------------------------------------
+
+__New in Delete Key for Gmail™ 1.0:__
+
+* Initial release
+
+---------------------------------------------------------
+
+Gmail is a trademark of Google LLC. Use of this trademark is subject to Google Permissions. This extension and its developers are in no way associated with Google LLC.
---
+++ js/contentscript.js
@@ -0,0 +1,50 @@
+// Code for triggering clicks on items loaded through AJAX
+// Source: https://stackoverflow.com/a/15512019
+function triggerMostButtons (jNode) {
+    triggerMouseEvent (jNode, "mouseover");
+    triggerMouseEvent (jNode, "mousedown");
+    triggerMouseEvent (jNode, "mouseup");
+    triggerMouseEvent (jNode, "click");
+}
+
+function triggerMouseEvent (node, eventType) {
+    var clickEvent = document.createEvent('MouseEvents');
+    clickEvent.initEvent (eventType, true, true);
+    node.dispatchEvent (clickEvent);
+}
+
+// Function for deleting the message
+function deleteMail() {
+    // Find out what element is currently in focus
+    try {
+        var focusedElement = document.activeElement;
+    } catch (err) {
+        console.log("Error finding focused element: " + err)
+    }
+    // Simulate click on dropdown menu
+    triggerMostButtons(document.querySelector('div[data-tooltip="More"]'));
+    // Simulate click on 'Delete this message' menu item
+    var deleteMenuItem = document.querySelector('div[class="b7 J-M"] #tm');
+    // This needs to be triggered twice for some reason
+    triggerMostButtons(deleteMenuItem);
+    triggerMostButtons(deleteMenuItem);
+    // Restore focus to original element
+    try {
+        focusedElement.focus();
+    } catch (err) {
+        console.log("Error restoring element focus: " + err)
+    }
+}
+
+// Set listener for shortcut
+chrome.storage.sync.get({
+    data: ["shiftKey", "53"]
+}, function(items) {
+    document.addEventListener('keydown', function doc_keyUp(e) {
+        if ((items.data[0] == "noKey") && (e.keyCode == items.data[1])) {
+            deleteMail();
+        } else if ((e[items.data[0]]) && (e.keyCode == items.data[1])) {
+            deleteMail();
+        }
+    });
+});
